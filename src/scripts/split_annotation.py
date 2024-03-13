f52db57befdfeb2086e8b450edb921012409a83f
Anita Caron <anitacaron@users.noreply.github.com>
2024-03-13 16:49:36 +0000

    python script to split annotations

--- src/scripts/split_annotation.py
+++ src/scripts/split_annotation.py
@@ -1,28 +1,31 @@
-from rdflib import Graph, Namespace, URIRef
+from rdflib import Graph, Namespace, URIRef, Literal
 from rdflib.plugins.sparql import prepareQuery
 
 
-g = Graph().parse(location="afpo-edit.owl")
+g = Graph().parse(location="ontology/afpo-edit.owl")
 
-prep_query = prepareQuery (
+prep_query = prepareQuery(
     "SELECT ?term ?value WHERE { ?term ?annotation ?value . }",
     initNs={"AfPO": Namespace("http://purl.obolibrary.org/obo/AfPO_")}
 )
 
 
-def query_annotation(annotation: str):
-    qres = g.query(prep_query, initBindings={"annotation": URIRef(annotation)})
-    for row in qres:
-        term, value = row
-        
-        
-        
+def split_annotation():
+    annotations = [
+        "http://purl.obolibrary.org/obo/AfPO_0000450",
+        "http://purl.obolibrary.org/obo/AfPO_0000565"
+    ]
+    g_annotations = Graph()
+    for an in annotations:
+        qres = g.query(prep_query, initBindings={"annotation": URIRef(an)})
+        for row in qres:
+            t, v = row
+            for value in v.split(", "):
+                g_annotations.add((URIRef(t), URIRef(an), Literal(value)))
+            g.remove((URIRef(t), URIRef(an), Literal(v)))
+
+    g_annotations.serialize(destination="annotations.owl", format="xml")
+    g.serialize(destination="afpo-edit-annotation.owl", format="xml")
 
 
-# v = g.serialize(format="xml")
-annotations = [
-    "http://purl.obolibrary.org/obo/AfPO_0000450"
-]
-
-for an in annotations:
-    query_annotation(an)
+split_annotation()
