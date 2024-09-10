c0736a0a74079ef0bd9da42dc8537d304dc3b029
Anita Caron <anitacaron@users.noreply.github.com>
2024-09-10 16:46:05 +0100

    update python script to split `language:synonym`

--- src/scripts/split_annotation.py
+++ src/scripts/split_annotation.py
@@ -1,6 +1,5 @@
-from rdflib import Graph, Namespace, URIRef, Literal
+from rdflib import Graph, Literal, Namespace, URIRef
 from rdflib.plugins.sparql import prepareQuery
-
 
 g = Graph().parse(location="ontology/afpo-edit.owl")
 
@@ -12,16 +11,27 @@ prep_query = prepareQuery(
 
 def split_annotation():
     annotations = [
-        "http://purl.obolibrary.org/obo/AfPO_0000450",
-        "http://purl.obolibrary.org/obo/AfPO_0000565"
+        "http://purl.obolibrary.org/obo/AfPO_0000453",
+        # "http://purl.obolibrary.org/obo/AfPO_0000450",
+        # "http://purl.obolibrary.org/obo/AfPO_0000565"
     ]
+    sep_cases = (
+        ", and ",
+        ", OR ",
+        ", or ",
+        " or ",
+        " / ",
+        ", "
+    )
     g_annotations = Graph()
     for an in annotations:
         qres = g.query(prep_query, initBindings={"annotation": URIRef(an)})
         for row in qres:
             t, v = row
-            for value in v.split(", "):
-                g_annotations.add((URIRef(t), URIRef(an), Literal(value)))
+            for case in sep_cases:
+                values = v.split(case)
+                for value in values:
+                    g_annotations.add((URIRef(t), URIRef(an), Literal(value)))
             g.remove((URIRef(t), URIRef(an), Literal(v)))
 
     g_annotations.serialize(destination="annotations.owl", format="xml")
